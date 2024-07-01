f3b0cf834679958c87d211373a6cfd6a51cd197f
AminVost <aminvost@gmail.com>
2024-07-01 16:11:55 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+import "reactflow/dist/style.css";
+import Home from "./Pages/Homepage/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
